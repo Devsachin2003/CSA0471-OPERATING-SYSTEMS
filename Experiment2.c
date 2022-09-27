a39856b53413e1669f65813aa7ef38649d162348
Devsachin2003 <112550872+Devsachin2003@users.noreply.github.com>
2022-09-27 22:24:36 +0530

    Update Experiment2.c

--- Experiment2.c
+++ Experiment2.c
@@ -1,44 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h> // For exit()
-
+#include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-	FILE *fptr1, *fptr2;
-	char filename[100], c;
-
-	printf("Enter the filename to open for reading \n");
-	scanf("%s", filename);
-
-	// Open one file for reading
-	fptr1 = fopen(filename, "r");
-	if (fptr1 == NULL)
-	{
-		printf("Cannot open file %s \n", filename);
-		exit(0);
-	}
-
-	printf("Enter the filename to open for writing \n");
-	scanf("%s", filename);
-
-	// Open another file for writing
-	fptr2 = fopen(filename, "w");
-	if (fptr2 == NULL)
-	{
-		printf("Cannot open file %s \n", filename);
-		exit(0);
-	}
-
-	// Read contents from file
-	c = fgetc(fptr1);
-	while (c != EOF)
-	{
-		fputc(c, fptr2);
-		c = fgetc(fptr1);
-	}
-
-	printf("\nContents copied to %s", filename);
-
-	fclose(fptr1);
-	fclose(fptr2);
-	return 0;
+FILE *fptr1,*fptr2;
+char filename[30],c;
+printf("enter the name of the file to be opened for reading:\n");
+scanf("%s",filename);
+fptr1=fopen(filename,"r");
+if(fptr1==NULL)
+{
+	printf("unable to open the file %s",filename);
+	exit(0);
+}
+printf("enter the name of the file to be opened for writing:\n");
+scanf("%s",filename);
+fptr2=fopen(filename,"w");
+if(fptr2==NULL)
+{
+	printf("unable to open the file %s",filename);
+	exit(0);
+}
+c=fgetc(fptr1);
+while(c!=EOF)
+{
+	fputc(c,fptr2);
+	c=fgetc(fptr1);
+}
+printf("content copied\n");
+fclose(fptr1);
+fclose(fptr2);
+return 0;
 }
